4373d4869ca846a50ab5d84da963469b5158ca1b
fabriciosuzart <fabriciosuzart.andrade@gmail.com>
2026-08-11 20:50:11 -0300

    Separa corretamente o postcss e o next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Só ativa o cache forte no Deploy final
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Suas outras configurações do Next ficam aqui, se houver
+  turbopack: {},
 };
 
 export default withPWA(nextConfig);
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,19 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
-
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {}, // <--- Adiciona esta linha para silenciar o erro do Turbopack na Vercel
-};
-
-export default withPWA(nextConfig);
+export default config;
