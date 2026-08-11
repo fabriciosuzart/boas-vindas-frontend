9799aa4c52734d94abdd4bb8740a36f34383169d
fabriciosuzart <fabriciosuzart.andrade@gmail.com>
2026-08-11 20:46:00 -0300

    Adiciona configuracao do turbopack no next.config.mjs para a Vercel

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
 
-export default config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {}, // <--- Adiciona esta linha para silenciar o erro do Turbopack na Vercel
+};
+
+export default withPWA(nextConfig);
